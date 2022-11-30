5296195de73f5e19230129d44fc1712939769192
Puneet Sharma <puneet438@gmail.com>
2022-11-30 15:00:32 +0530

    saving...

---
+++ codes/gsap/gsap1.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+// ease set to back, elastic, and other - https://greensock.com/docs/v3/Eases
+gsap.to(".logo", {duration:2, x: 300, backgroundColor:"#560563", borderRadius: "50%", border: "5px solid white", ease: "back"});
+
---
+++ codes/react/react1.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Style inline attribute
+style={{ flexGrow: "1", background: "rgba(0, 0, 0, 0.06)", paddingLeft: "10px" }}
