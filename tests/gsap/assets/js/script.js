f2096fb06d462cd6ef9e94d73a5cb5113c2ef879
Puneet Sharma <puneet438@gmail.com>
2022-12-02 14:39:08 +0530

    saving...

--- tests/gsap/assets/js/script.js
+++ tests/gsap/assets/js/script.js
@@ -3,5 +3,10 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".square", {
     x: 700,
     duration: 3,
-    scrollTrigger: ".div3"
+    scrollTrigger: {
+        trigger: ".square", 
+        start: "center center",
+        markers: true,
+        scrub: true,
+    }
 })
