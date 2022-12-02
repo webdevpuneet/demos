e7e7608a5a0341283e816e7f5078506e0e8a55af
Puneet Sharma <puneet438@gmail.com>
2022-12-02 13:13:55 +0530

    saving...

--- codes/gsap/gsap1.js
+++ codes/gsap/gsap1.js
@@ -1,7 +1,8 @@
 // Install Plugins - https://greensock.com/docs/v3/Installation
 
 
-
+// Register plugin 
+gsap.registerPlugin(ScrollTrigger);
 
 // ease set to back, elastic, and other - https://greensock.com/docs/v3/Eases
 gsap.to(".logo", {duration:2, x: 300, backgroundColor:"#560563", borderRadius: "50%", border: "5px solid white", ease: "back"});
