141780c84c7e583b6f0bbbb85134809b7b82ff66
Puneet Sharma <puneet438@gmail.com>
2022-12-09 12:47:22 +0530

    adding timeline to demo

--- codes/gsap/gsap1.js
+++ codes/gsap/gsap1.js
@@ -79,4 +79,30 @@ gsap.to(".square", {
     }  
 */
 
+/**
+ * Timeline
+ * 
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".square2", 
+        start: "top 60%",
+        end: "top 30%",
+        markers: {
+            startColor: "green",
+            endColor: "red",
+            fontSize: "1rem",
+        },
+        toggleClass: 'red',
+        toggleActions: 'restart reverse restart reverse',
+        pin: '.square',
+        pinSpacing: true,
+    }  
+})
+
+tl.to(".square", {x: 500, duration: 2})
+  .to(".square", {y: 200, duration: 2})
+  .to(".square", {x: 0, duration: 2})
+  
+ */
+
 
--- demos/demos0/gsap/scrolltrigger-timeline/assets/js/script.js
+++ demos/demos0/gsap/scrolltrigger-timeline/assets/js/script.js
@@ -1,24 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".square", {
-//     x: 700,
-//     duration: 3,
-//     scrollTrigger: {
-//         trigger: ".square2", 
-//         start: "top 60%",
-//         end: "top 30%",
-//         markers: {
-//             startColor: "green",
-//             endColor: "red",
-//             fontSize: "1rem",
-//         },
-//         toggleClass: 'red',
-//         toggleActions: 'restart reverse restart reverse',
-//         pin: '.square',
-//         pinSpacing: true,
-//         // onEnter onLeave onEnterBack onLeaveBack
-//     }   
-// })
 
 const tl = gsap.timeline({
     scrollTrigger: {
